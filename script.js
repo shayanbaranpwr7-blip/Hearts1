cece3ca3a6668760ada697dece656ef8b9301f54
shayanbaranpwr7-blip <shayanbaranpwr7@gmail.com>
2026-07-31 23:30:45 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 const button = document.getElementById("startButton");
+const welcome = document.querySelector(".welcome");
+
 
 button.addEventListener("click", () => {
-    button.innerText = "Welcome 🤍";
+
+    button.innerText = "Entering... 🤍";
+
+
+    welcome.style.transition = "2s";
+
+    welcome.style.opacity = "0";
+
+    welcome.style.transform = "scale(0.8)";
+
+
+    setTimeout(() => {
+
+        button.innerText = "Welcome 🤍";
+
+        welcome.style.opacity = "1";
+
+        welcome.style.transform = "scale(1)";
+
+
+    }, 2500);
+
+
 });
