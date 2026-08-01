766685463a8c62a1f8d47a18eb625829aba56a53
shayanbaranpwr7-blip <shayanbaranpwr7@gmail.com>
2026-08-01 05:34:10 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,54 +1,18 @@
 const button = document.getElementById("startButton");
 const welcome = document.querySelector(".welcome");
 
-const rain = document.getElementById("rain");
-
-
-for (let i = 0; i < 120; i++) {
-
-    const drop = document.createElement("div");
-
-    drop.className = "raindrop";
-
-
-    drop.style.left = Math.random() * 100 + "%";
-
-    drop.style.animationDuration =
-        (0.5 + Math.random() * 1.5) + "s";
-
-
-    drop.style.animationDelay =
-        Math.random() * 5 + "s";
-
-
-    rain.appendChild(drop);
-
-}
-
-
-
 button.addEventListener("click", () => {
-
+    button.disabled = true;
     button.innerText = "Entering... 🤍";
 
-
-    welcome.style.transition = "2s";
-
+    welcome.style.transition = "1.8s ease";
     welcome.style.opacity = "0";
-
-    welcome.style.transform = "scale(0.8)";
-
+    welcome.style.transform = "scale(0.95)";
 
     setTimeout(() => {
-
-        welcome.style.opacity = "1";
-
-        welcome.style.transform = "scale(1)";
-
         button.innerText = "Welcome 🤍";
-
-
-    }, 2500);
-
-
+        welcome.style.opacity = "1";
+        welcome.style.transform = "scale(1)";
+        button.disabled = false;
+    }, 1800);
 });
